165503ffacb407585d1ee3f4638ebdc64882bd82
CGH <tt878429@gmail.com>
2023-05-15 23:55:41 +0900

    chore: ESLint, Prettier 세팅

--- components/provider/ReactQuery.tsx
+++ components/provider/ReactQuery.tsx
@@ -4,5 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function ReactQuery({ children }: Props) {
   const queryClient = new QueryClient();
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 }
