697064ce55aa2b4c4216eec169caa476f3698084
CGH <tt878429@gmail.com>
2023-05-20 22:01:10 +0900

    fix: ESLint rule에 맞게 코드 수정

--- app/sample.test.tsx
+++ app/sample.test.tsx
@@ -1,6 +1,7 @@
-import Home from "./page";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+
+import Home from "./page";
 
 function sampleFunction(a: number, b: number) {
   return a + b;
--- components/layout/Header/HeaderLogo.tsx
+++ components/layout/Header/HeaderLogo.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import logo from "../../../public/images/logo.png";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+
+import logo from "../../../public/images/logo.png";
 
 export default function HeaderLogo() {
   const router = useRouter();
