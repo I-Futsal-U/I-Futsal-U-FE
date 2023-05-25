eb521f5776cd1c966d5092338e49c3ee0aa3d886
CGH <tt878429@gmail.com>
2023-05-25 16:36:38 +0900

    design: Header z-index 20으로 변경

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -5,7 +5,7 @@ import HeaderMypage from "./HeaderMypage";
 
 export default function Header() {
   return (
-    <header className="fixed top-0 flex flex-row justify-start md:justify-between md:ml-0 items-center bg-gray-900 h-20 opacity-90 w-full">
+    <header className="fixed z-20 top-0 flex flex-row justify-start md:justify-between md:ml-0 items-center bg-gray-900 h-20 opacity-90 w-full">
       <HeaderLogo />
       <HeaderInputBar />
       <HeaderLogin />
