7cbb382a881e1468634a3bce93bb0d686fce7ddd
JeonInguk <inguk12951@gmail.com>
2023-05-29 07:04:35 +0900

    Feat:검색된 리스트 목록표시할 박스생성

--- components/common/Map/Map.tsx
+++ components/common/Map/Map.tsx
@@ -1,6 +1,8 @@
 import temporarydata from "./temporarydata.json";
 import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbTextWrapDisabled } from "react-icons/tb";
 
 interface PlaceSearch {
   data: { [key: string]: string }[];
@@ -10,6 +12,8 @@ interface PlaceSearch {
 
 export default function Map() {
   const { data: inputValue } = useQuery(["inputValue"]);
+  const [listItems, setListItems] = useState([]);
+  const [toggle, setToggle] = useState(true);
 
   useEffect(() => {
     const mapScript = document.createElement("script");
@@ -18,6 +22,7 @@ export default function Map() {
     document.head.appendChild(mapScript);
     const onLoadKakaoMap = () => {
       window.kakao.maps.load(() => {
+        let markers = [];
         const mapContainer = document.getElementById("map");
         const mapOption = {
           center: new window.kakao.maps.LatLng(37.541, 126.986),
@@ -35,59 +40,86 @@ export default function Map() {
         // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
         const zoomControl = new window.kakao.maps.ZoomControl();
         map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
-
         const positions = temporarydata.DATA.map((item) => ({
           title: item.svcnm,
           latlng: new window.kakao.maps.LatLng(item.y, item.x),
         }));
 
-        // 마커 이미지의 이미지 주소입니다
-        const imageSrc =
-          "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-
-        for (let i = 0; i < positions.length; i++) {
-          // 마커 이미지의 이미지 크기 입니다
-          const imageSize = new window.kakao.maps.Size(24, 35);
-
-          // 마커 이미지를 생성합니다
-          const markerImage = new window.kakao.maps.MarkerImage(
-            imageSrc,
-            imageSize,
-          );
-
-          // 마커를 생성합니다
-          const marker = new window.kakao.maps.Marker({
-            map: map, // 마커를 표시할 지도
-            position: positions[i].latlng, // 마커를 표시할 위치
-            title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-            image: markerImage, // 마커 이미지
-          });
-        }
-
         const ps = new window.kakao.maps.services.Places();
-        ps.keywordSearch(inputValue, placesSearchCB);
-        // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+
+        if (inputValue) {
+          ps.keywordSearch(inputValue, placesSearchCB);
+        }
         function placesSearchCB(
           data: PlaceSearch["data"],
           status: PlaceSearch["status"],
           pagination: PlaceSearch["pagination"],
         ) {
           if (status === window.kakao.maps.services.Status.OK) {
+            // 정상적으로 검색이 완료됐으면
+            // 검색 목록과 마커를 표출합니다
+            displayPlaces(data);
+            setListItems(data);
+
+            // 페이지 번호를 표출합니다
+          } else if (status === window.kakao.maps.services.Status.ZERO_RESULT) {
+            alert("검색 결과가 존재하지 않습니다.");
+            return;
+          } else if (status === window.kakao.maps.services.Status.ERROR) {
+            alert("검색 결과 중 오류가 발생했습니다.");
+            return;
+          }
+        }
+        // 검색 결과 목록과 마커를 표출하는 함수입니다
+        function displayPlaces(places) {
+          const fragment = document.createDocumentFragment(),
+            bounds = new window.kakao.maps.LatLngBounds(),
+            listStr = "";
+
+          // 지도에 표시되고 있는 마커를 제거합니다
+          removeMarker();
+
+          for (let i = 0; i < places.length; i++) {
+            // 마커를 생성하고 지도에 표시합니다
+            const placePosition = new window.kakao.maps.LatLng(
+                places[i].y,
+                places[i].x,
+              ),
+              marker = addMarker(placePosition, i);
+
             // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
             // LatLngBounds 객체에 좌표를 추가합니다
-            const bounds = new window.kakao.maps.LatLngBounds();
-            console.log(data);
-
-            if (data) {
-              for (let i = 0; i < data.length; i++) {
-                bounds.extend(
-                  new window.kakao.maps.LatLng(data[i].y, data[i].x),
-                );
-              }
-            }
-            // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-            map.setBounds(bounds);
+            bounds.extend(placePosition);
           }
+
+          // 검색결과 항목들을 검색결과 목록 Element에 추가합니다
+
+          // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+          map.setBounds(bounds);
+        }
+        function removeMarker() {
+          for (let i = 0; i < markers.length; i++) {
+            markers[i].setMap(null);
+          }
+          markers = [];
+        }
+        function addMarker(position, idx, title) {
+          const imageSrc =
+              "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png",
+            imageSize = new window.kakao.maps.Size(24, 35), // 마커 이미지의 크기
+            markerImage = new window.kakao.maps.MarkerImage(
+              imageSrc,
+              imageSize,
+            ),
+            marker = new window.kakao.maps.Marker({
+              position: position, // 마커의 위치
+              image: markerImage,
+            });
+
+          marker.setMap(map); // 지도 위에 마커를 표출합니다
+          markers.push(marker); // 배열에 생성된 마커를 추가합니다
+
+          return marker;
         }
       });
     };
@@ -96,8 +128,39 @@ export default function Map() {
 
   return (
     <>
-      <div className="flex flex-row justify-center items-center">
-        <div id="map" className="w-screen h-screen"></div>
+      <div className="flex flex-row fixed">
+        {listItems.length === 0 ? null : (
+          <div className="z-50 bg-black w-10 h-10 rounded-full relative flex items-center justify-center cursor-pointer">
+            <TbTextWrapDisabled
+              onClick={() => setToggle((prev) => !prev)}
+              className="text-white text-2xl absolute inset-0 m-auto"
+            />
+          </div>
+        )}
+        {toggle === true && listItems.length !== 0 ? (
+          <div
+            id="menu_wrap"
+            className="overflow-y-auto h-screen z-10 bg-gray-900 opacity-95 text-white w-72"
+            style={{ maxHeight: "calc(100vh - 80px)" }}
+          >
+            <ul id="placesList" className="flex flex-col pt-10">
+              <AiFillCloseCircle
+                onClick={() => setToggle((prev) => !prev)}
+                className="cursor-pointer"
+              />
+              {listItems.map((item) => {
+                return (
+                  <li key={item?.id} className="my-2 mb-10">
+                    <div>{item?.place_name}</div>
+                    <div>{item?.address_name}</div>
+                    <div>{item?.phone}</div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        ) : null}
+        <div id="map" className="w-screen h-screen z-0 absolute top-0 left-0" />
       </div>
     </>
   );
