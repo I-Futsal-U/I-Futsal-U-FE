f4a7b30efe5706009834bca6423c2f3507a7b633
CGH <tt878429@gmail.com>
2023-06-28 11:05:02 +0900

    Fix: button => div 태그로 되돌림

--- components/searchPage/PlaceList.tsx
+++ components/searchPage/PlaceList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 type PlaList = {
   listItems: { [key: string]: string }[];
   toggle: () => void;
@@ -7,16 +8,15 @@ export default function PlaceList({ listItems, toggle }: PlaList) {
   return (
     <>
       {listItems.map((item) => (
-        <button
+        <div
           key={item.id}
           className="py-3 md:py-6 px-5 border-t-2 border-zinc-600 cursor-pointer"
           onClick={toggle}
-          type="button"
         >
           <div className="text-base md:text-xl pb-1 ">{item?.place_name}</div>
           <div className="text-xs md:text-sm pb-1">{item?.address_name}</div>
           <div className="text-xs md:text-sm text-zinc-400">{item?.phone}</div>
-        </button>
+        </div>
       ))}
     </>
   );
